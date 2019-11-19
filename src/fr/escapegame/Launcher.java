c1c604c0beb173c696ea0e9688922387f1af081c
anthonyglair <anthonyglairon@gmail.com>
2019-11-19 22:22:52 +0100

    Maj du 19/11/2019

--- src/fr/escapegame/Launcher.java
+++ src/fr/escapegame/Launcher.java
@@ -1,3 +1,4 @@
+
 package fr.escapegame;
 
 import java.util.InputMismatchException;
@@ -11,15 +12,14 @@ public class Launcher {
 
 	public static void main(String[] args) {
 
-		LOGGER.info("Le nombre de combinaison est " + ChargerPropriete.NB_COMBINAISON);
-		LOGGER.info("Le nombre d'essais est " + ChargerPropriete.NB_ESSAI);
-		LOGGER.info("Activation du mode développeur: " + ChargerPropriete.MODE_DEV);
-		LOGGER.info("");
+		System.out.println("Le nombre de combinaison est " + ChargerPropriete.NB_COMBINAISON);
+		System.out.println("Le nombre d'essais est " + ChargerPropriete.NB_ESSAI);
+		System.out.println("Activation du mode développeur: " + ChargerPropriete.MODE_DEV);
+		System.out.println("");
 		reglement();
 		ModeDeJeux jeu = selectionDuModeDeJeux();
 		jeu.jouer();
-		jeu.presentationIa();
-
+		
 	}
 
 	/**
@@ -27,19 +27,18 @@ public class Launcher {
 	 */
 
 	public static void reglement() {
-		LOGGER.info("Début de partie");
-		LOGGER.info(" Bonjour :-) \n\r Si tu as pris connaissance des règles de jeux nous pouvons commencer tout de suite. \n\r ");
-		LOGGER.info(" Tout d'abord fais ton choix parmi les 3 modes de jeux ci-dessous:");
-		LOGGER.info("");
+		System.out.println(" Bonjour :-) \n\r Si tu as pris connaissance des règles de jeux nous pouvons commencer tout de suite. \n\r ");
+		System.out.println(" Tout d'abord fais ton choix parmi les 3 modes de jeux ci-dessous:");
+		System.out.println("");
 		
 	}
 
 	public static void presentationDesModes() {
 
-		LOGGER.info(" 1 - Le mode Challenger  ");
-		LOGGER.info(" 2 - Le mode Defenseur   ");
-		LOGGER.info(" 3 - Le mode Duel        ");
-		LOGGER.info(" 4 - Sortie de la partie ");
+		System.out.println(" 1 - Le mode Challenger  ");
+		System.out.println(" 2 - Le mode Defenseur   ");
+		System.out.println(" 3 - Le mode Duel        ");
+		System.out.println(" 4 - Sortie de la partie ");
 
 	}
 
@@ -55,29 +54,29 @@ public class Launcher {
 			int nbMode = sc.nextInt();
 			switch (nbMode) {
 			case 1:
-				LOGGER.debug(" Tu as choisi le mode Challenger");
+				System.out.println(" Tu as choisi le mode Challenger");
 				jeu = new ModeChallenger();
 				break;
 			case 2:
-				LOGGER.debug(" Tu as choisi le mode Defenseur");
+				System.out.println(" Tu as choisi le mode Defenseur");
 				jeu = new ModeDefenseur();
 				break;
 			case 3:
-				LOGGER.debug(" Tu as choisi le mode Duel");
+				System.out.println(" Tu as choisi le mode Duel");
 				jeu = new ModeDuel();
 				break;
 			case 4:
-				LOGGER.debug("fin de partie");
+				System.out.println("fin de partie");
 				System.exit(0);
 				break;
 			default:
-				LOGGER.debug("Erreur de saisie, recommence. ");
+				System.out.println("Erreur de saisie, recommence. ");
 				selectionDuModeDeJeux();
 				break;
 			}
 
 		} catch (InputMismatchException e) {
-			LOGGER.debug("Erreur de saisie, recommence");
+			System.out.println("Erreur de saisie, recommence");
 			selectionDuModeDeJeux();
 
 		}
