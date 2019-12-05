8884f459e9c333ab0509153c772dde3e06aca062
anthonyglair <anthonyglairon@gmail.com>
2019-12-05 23:19:23 +0100

    Ajout de la comparaison entre la saisie joueur et la combinaison aléatoire

--- src/fr/escapegame/ModeChallenger.java
+++ src/fr/escapegame/ModeChallenger.java
@@ -15,50 +15,53 @@ public class ModeChallenger extends ModeDeJeux {
 
 	/*
 	 * @param combinaisonIA Génère une combinaison aléatoire entre 0 et 9 de x
-	 * chiffresdéterminé par la variable nbcombinaison
-	 * Activation ou non de la visibilité de la combinaison
+	 * chiffresdéterminé par la variable nbcombinaison Activation ou non de la
+	 * visibilité de la combinaison
 	 * 
 	 */
 	public void combiAleatoire() {
 		this.combinaison = new int[nbCombinaison];
 		for (int i = 0; i < nbCombinaison; i++) {
 			combinaison[i] = (int) (Math.random() * 9);
-		}
-		for (int j = 0; j < combinaison.length; j++)
 			if (modeDev == true) {
-				System.out.print(combinaison[j]);
-			} else {
-				System.out.println();
-
-			}
+			System.out.print(combinaison[i]);
+		}}
 	}
-
+	/*méthode retournant la combinaison*/
+	/*public int combiVisible( ) {
+		this.combiAleatoire();*/
+		
+		
+		
+	
+			
+	
+		
 	/*
 	 * @param saisieJoueur Méthode qui permet à l'utilisateur de saisir une
 	 * combinaison
 	 */
 
 	public void saisieJoueur() {
-
+		int nbr = 0;
 		System.out.println();
 		System.out.println();
 		Scanner sc = new Scanner(System.in);
-		/* On saisi une phrase */
 		System.out.println("Saisir " + nbCombinaison + " chiffres ");
-		/* La sasie scanner est transféré dans une variable nb */
 		String nb = sc.nextLine();
 		char[] tab = nb.toCharArray();
-		int[] saisie = new int[tab.length];
+		int saisie = 0;
 		for (int k = 0; k < tab.length; k++) {
-			saisie[k] = Integer.parseInt(String.valueOf(tab[k]));
-			System.out.println("");
-			 System.out.println(saisie[k]); 
-			for (int z = 0; z < 1 ; z++) {
-				System.out.print(combinaison[z]);
-
-			}
+			saisie = Integer.parseInt(String.valueOf(tab[k]));
+			if (combinaison[k] == saisie) {
+				System.out.print("=");
+			} else if (saisie < combinaison[k]) {
+				System.out.print("-");
+			} else {
+				System.out.print("+");
+				}
 		}
-	
+	}
 	/* System.out.print("nombre" + combinaison[z]); */
 
 	public void jouer() {
