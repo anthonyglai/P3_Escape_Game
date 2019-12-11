8babb09cad1e3c214d7bd7ca1a4e5405ae91d772
anthonyglair <anthonyglairon@gmail.com>
2019-12-11 20:18:35 +0100

    Ajout des méthodes tentativeDeTrouverLaCombinaison et propositionApresUneFinDePartie

--- src/fr/escapegame/ModeChallenger.java
+++ src/fr/escapegame/ModeChallenger.java
@@ -1,6 +1,7 @@
 package fr.escapegame;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,8 +16,8 @@ public class ModeChallenger extends ModeDeJeux {
 	private char[] tab;
 	private int saisie = 0;
 	private int combinaisonAleatoire = 0;
-	
-	
+	private int tentative = 0;
+
 	/*
 	 * @param combinaisonIA Génère une combinaison aléatoire entre 0 et 9 de x
 	 * chiffresdéterminé par la variable nbcombinaison Activation ou non de la
@@ -28,17 +29,18 @@ public class ModeChallenger extends ModeDeJeux {
 		for (int i = 0; i < nbCombinaison; i++) {
 			combinaison[i] = (int) (Math.random() * 9);
 			if (modeDev == true) {
-			System.out.print(combinaison[i]);
-		}}
+				System.out.print(combinaison[i]);
+			}
+		}
 	}
-	/*méthode retournant la combinaison*/
-	public void combiVisible( ) {
+
+	/* méthode retournant la combinaison */
+	public void combinaisonVisible() {
 		System.out.println("");
 		System.out.println("La combinaison à deviner est :");
 		this.combiAleatoire();
 	}
 
-			
 	/*
 	 * @param saisieJoueur Méthode qui permet à l'utilisateur de saisir une
 	 * combinaison
@@ -52,47 +54,76 @@ public class ModeChallenger extends ModeDeJeux {
 		String nb = sc.nextLine();
 		tab = nb.toCharArray();
 		System.out.println("Vous avez saisi " + nb);
-		
+
 	}
-		
-	public void comparaisonDeCombinaison() {	
-	
+
+	/*
+	 * @param comparaisonDeCombinaison Méthode qui compare la saisie joueur à l
+	 * combinaison aléatoire
+	 * 
+	 */
+
+	public void comparaisonDeCombinaison() {
+
 		System.out.println("Le resultat est le suivant");
 		for (int k = 0; k < tab.length; k++) {
 			saisie = Integer.parseInt(String.valueOf(tab[k]));
 			combinaisonAleatoire = combinaison[k];
-			if (saisie == combinaisonAleatoire ) {
+			if (saisie == combinaisonAleatoire) {
 				System.out.print("=");
 			} else if (saisie < combinaisonAleatoire) {
 				System.out.print("-");
 			} else {
 				System.out.print("+");
-			
+
 			}
-			
-		}}
-		public void test() {
-			System.out.println("");
-			if ( saisie == combinaisonAleatoire ) {
-				System.out.println( "vous avez gagné");
+
 		}
-	
 	}
-	
-	
-	
-		
-	
-	
-		
-	
-	/* System.out.print("nombre" + combinaison[z]); */
+
+	/*
+	 * @param tentativeDeTrouverLaCombinaison Méthode permettant au joueur de faire
+	 * une saisie de la comparer à la combinaison aléatoire autant de fois que
+	 * nombre d'essai paramétré dans le fichier de propriété
+	 */
+	public void tentativeDeTrouverLaCombinaison() {
+		do {
+			this.saisieJoueur();
+			this.comparaisonDeCombinaison();
+			System.out.println("");
+			System.out.println("");
+			if (saisie == combinaisonAleatoire) {
+				System.out.print("Vous avez gagné");
+			} else if (saisie < combinaisonAleatoire) {
+				System.out.println("Vous n'avez pas trouvé la combinaison exact, veuillez rententer votre chance.");
+			} else
+				System.out.println("Vous n'avez pas trouvé la combinaison exact, veuillez retenter votre chance.");
+		} while (saisie != combinaisonAleatoire & tentative < nbEssai);
+
+	}
+
+	/*
+	 * @ propositionApresUneFinDePartie Méthode proposant plusieurs choix après la
+	 * fin d'une partie
+	 */
+
+	public void propositionApresUneFinDePartie() {
+		System.out.println("");
+		System.out.println("");
+		System.out.println(" Maintenant que la partie est fini ");
+		System.out.println("");
+		System.out.println(" Veuillez saisir un numéro parmi les 3 choix");
+		System.out.println("");
+		System.out.println(" 1- Recommencer une partie ");
+		System.out.println(" 2- Changer de mode de jeu");
+		System.out.println(" 3- Quitter la partie");
+
+	}
 
 	public void jouer() {
-		combiVisible();
-		saisieJoueur();
-		comparaisonDeCombinaison();
-		test();
-	
+		combinaisonVisible();
+		tentativeDeTrouverLaCombinaison();
+		propositionApresUneFinDePartie();
+
 	}
 }
