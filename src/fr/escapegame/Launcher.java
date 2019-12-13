c9a12ae31b2fef90280fcf1308e54e8d2333be33
anthonyglair <anthonyglairon@gmail.com>
2019-12-13 17:10:58 +0100

    Changement de la méthode static en méthode non static

--- src/fr/escapegame/Launcher.java
+++ src/fr/escapegame/Launcher.java
@@ -1,4 +1,3 @@
-
 package fr.escapegame;
 
 import java.util.InputMismatchException;
@@ -7,37 +6,25 @@ import org.apache.log4j.Logger;
 import fr.escapegame.propriete.ChargerPropriete;
 
 public class Launcher {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(Launcher.class);
 
-	public static void main(String[] args) {
+	public void paramétrage() {
 
-		
 		System.out.println("Le nombre de combinaison est " + ChargerPropriete.NB_COMBINAISON);
 		System.out.println("Le nombre d'essais est " + ChargerPropriete.NB_ESSAI);
 		System.out.println("Activation du mode développeur: " + ChargerPropriete.MODE_DEV);
 		System.out.println("");
-		reglement();
-		ModeDeJeux jeu = selectionDuModeDeJeux();
-		jeu.jouer();
-		
+
 	}
 
 	/*
-	 * @param règlement
-	 * Le règlement du jeu
+	 * @param presentationDesModes La présentation des modes de jeux
 	 */
+	public void presentationDesModes() {
 
-	public static void reglement() {
-		System.out.println("Veuillez choisir parmi les 3 modes de jeux ci-dessous. \n\r ");
-			
-	}
-
-	/*
-	 * @param presentationDesModes
-	 * La présentation des modes de jeux
-	 */
-	public static void presentationDesModes() {
+		System.out.println("Veuillez choisir parmi les 3 modes de jeux ci-dessous.");
+		System.out.println("\n");
 
 		System.out.println(" 1 - Le mode Challenger  ");
 		System.out.println(" 2 - Le mode Defenseur   ");
@@ -45,36 +32,36 @@ public class Launcher {
 		System.out.println(" 4 - Sortie de la partie ");
 
 	}
-	
+
 	/*
-	 * @param selectionDuModeDeJeux
-	 * Présentation des 3 modes de jeux 
-	 * La selection est retourné dans le mode de jeu
+	 * @param selectionDuModeDeJeux Présentation des 3 modes de jeux La selection
+	 * est retourné dans le mode de jeu
 	 * 
 	 */
 
-	public static ModeDeJeux selectionDuModeDeJeux() {
-
+	public void ModeDeJeux() {
+		paramétrage();
 		presentationDesModes();
 
 		Scanner sc = new Scanner(System.in);
-		ModeDeJeux jeu = null;
-		
-		
+
 		try {
 			int nbMode = sc.nextInt();
 			switch (nbMode) {
 			case 1:
 				System.out.println("Tu as choisi le mode Challenger");
-				jeu = new ModeChallenger();
+				ModeChallenger modeChallenger = new ModeChallenger();
+				modeChallenger.jouer();
 				break;
 			case 2:
 				System.out.println("Tu as choisi le mode Defenseur");
-				jeu = new ModeDefenseur();
+				ModeChallenger modeDefenseur = new ModeDefenseur();
+				modeDefenseur.jouer();
 				break;
 			case 3:
 				System.out.println(" Tu as choisi le mode Duel");
-				jeu = new ModeDuel();
+				ModeDuel modeDuel = new ModeDuel();
+				modeDuel.jouer();
 				break;
 			case 4:
 				System.out.println("fin de partie");
@@ -82,21 +69,15 @@ public class Launcher {
 				break;
 			default:
 				System.out.println("Recommence ta saisie il n y a que 4 possibilité :");
-				selectionDuModeDeJeux();
+				ModeDeJeux();
 				break;
 			}
 
 		} catch (InputMismatchException e) {
 			System.out.println("Erreur de saisie, recommence");
-			selectionDuModeDeJeux();
+			ModeDeJeux();
 
 		}
-
-		/*
-		 * @return jeu
-		 * Retourne la selection du mode de jeux
-		 */
-		return jeu;
 
 	}
 }
