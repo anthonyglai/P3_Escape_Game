f66a02903db92d9622564910c16cfc9e2c25ff83
anthonyglair <anthonyglairon@gmail.com>
2019-12-13 17:12:00 +0100

    Création de la méthode main qui lance la méthode Launcher avec les 3 modes de jeux

---
+++ src/fr/escapegame/Main.java
@@ -0,0 +1,16 @@
+package fr.escapegame;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import fr.escapegame.propriete.ChargerPropriete;
+
+public class Main{
+
+	public static void main(String[] args) {
+		Launcher jeu = new Launcher();
+		jeu.ModeDeJeux();
+
+	}
+
+}
