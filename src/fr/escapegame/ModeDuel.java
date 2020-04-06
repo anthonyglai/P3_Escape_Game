41ec461843d0a2edd0492b695687675fde015ff1
anthonyglair <anthonyglairon@gmail.com>
2020-04-06 19:52:36 +0200

    Supression des méthodes choixApresUneFinDePartie et propositionApresUneFinDePartie

--- src/fr/escapegame/ModeDuel.java
+++ src/fr/escapegame/ModeDuel.java
@@ -9,27 +9,28 @@ import org.apache.log4j.Logger;
 
 import fr.escapegame.propriete.ChargerPropriete;
 
-    /**
-    * ModeDuel est la classe enfant de ModeDeJeux
-    * 
-    * @author Glairon Anthony
-    * @version 1.0
-    */
-    public class ModeDuel extends ModeDeJeux {    
-    
+/**
+ * ModeDuel est la classe enfant de ModeDeJeux
+ * 
+ * @author Glairon Anthony
+ * @version 1.0
+ */
+public class ModeDuel extends ModeDeJeux {
+
     public static final Logger LOGGER = Logger.getLogger(Launcher.class);
-    
+
     /**
      * Introduction du mode duel
-     */  
-    public void introduction() {                                                        
-        System.out.println("Le premier entre l'IA ou le joueur qui trouve la combinaison de " + getNbCombinaison() + " chiffre(s) a gagné. ");              
+     */
+    public void introduction() {
+        System.out.println("Le premier entre l'IA ou le joueur qui trouve la combinaison de " + getNbCombinaison()
+                + " chiffre(s) a gagné. ");
         System.out.println("Chaque participant à " + getNbEssai() + " essais. \n");
         System.out.println("La partie commence \n ");
     }
-    
+
     /**
-     * Methode pour que le joueur saisisse une combinaison de x chiffres 
+     * Methode pour que le joueur saisisse une combinaison de x chiffres
      */
     public void saisieCombinaisonSecreteJoueur() {
         Scanner scanner = new Scanner(System.in);
@@ -49,8 +50,7 @@ import fr.escapegame.propriete.ChargerPropriete;
         setTabDef(nbre.toCharArray());
         setCombinaisonSecreteJoueur(getCombinaisonSecreteJoueur() + (nbre));
         System.out.println("Vous avez saisi la combinaison secrète " + getCombinaisonSecreteJoueur());
-       
-        
+
     }
 
     /**
@@ -68,9 +68,9 @@ import fr.escapegame.propriete.ChargerPropriete;
             } else {
                 System.out.println("?");
             }
-          }
         }
-    
+    }
+
     /**
      * La methode affiche ou non la combinaison aleatoire de l IA si la variable
      * modeDev dans la methode combinaisonAleatoireIa est true
@@ -80,7 +80,7 @@ import fr.escapegame.propriete.ChargerPropriete;
         System.out.print("La combinaison de l'IA à deviner est ");
         this.combinaisonAleatoireIa();
     }
-    
+
     /**
      * La methode creee une combinaison aleatoire pour l IA entre 0 et 9 chiffres.
      * Mode defenseur La variable nbCombinaison determinera le nombre de chiffre que
@@ -94,25 +94,24 @@ import fr.escapegame.propriete.ChargerPropriete;
             setCombiIaConvertiEnString(getCombiIaConvertiEnString() + getCombinaisonIaAtt()[i]);
         }
     }
-    
-    /** 
+
+    /**
      * Methode pour indiquer que c est au tour de l IA de jouer
-     */   
+     */
     public void tourIa() {
         System.out.println("\n");
         System.out.println("C'est au tour de l' IA de faire une proposition \n ");
     }
 
-
     /**
-     * Methode qui affiche la combinaison aleatoire de l IA 
-     */   
+     * Methode qui affiche la combinaison aleatoire de l IA
+     */
     public void propositionCombinaisonIa() {
         System.out.println("Essai n° 1 \n");
         System.out.print("l'IA propose la combinaison ");
         this.creationCombinaisonAleatoireIa();
     }
-    
+
     /**
      * Cette methode compare la combinaison aleatoire de l IA a la combinaison
      * secrete du joueur et affiche un resultat sous forme d operateur
@@ -131,9 +130,9 @@ import fr.escapegame.propriete.ChargerPropriete;
                 setResultat(getResultat() + ("+"));
             }
         }
-        System.out.println(getResultat()+"\n");
+        System.out.println(getResultat() + "\n");
     }
-    
+
     /**
      * Methode affichant que l Ia a trouve ou non la combinaison du joueur
      */
@@ -146,17 +145,17 @@ import fr.escapegame.propriete.ChargerPropriete;
             System.out.println("L'ia n'a pas trouvé la combinaison du joueur \n ");
         }
     }
-    
-    /** 
+
+    /**
      * Methode pour annoncer que c est au tour du joueur de jouer
-     */    
+     */
     public void tourJoueur() {
         System.out.println("C'est au tour du joueur de faire une proposition");
     }
-    
+
     /**
-     * La methode permet au joueur de saisir une combinaison de x chiffres et 
-     * de l afficher
+     * La methode permet au joueur de saisir une combinaison de x chiffres et de l
+     * afficher
      */
     public void saisieJoueur() {
         setChanceUtiliseeJoueur(getChanceUtiliseeJoueur() + 1);
@@ -178,7 +177,7 @@ import fr.escapegame.propriete.ChargerPropriete;
         setTab(nb.toCharArray());
         System.out.println("Vous avez saisi " + nb);
     }
-    
+
     /**
      * La methode compare la saisie du joueur a la combinaison aleatoire de l IA Un
      * signe + - ou = indique au joueur si il a trouve ou non la combinaison de l IA
@@ -197,7 +196,7 @@ import fr.escapegame.propriete.ChargerPropriete;
             }
         }
     }
-    
+
     /**
      * Methode qui indique si le joueur a gagne ou perdu
      */
@@ -207,9 +206,9 @@ import fr.escapegame.propriete.ChargerPropriete;
             System.out.print("Vous avez gagné, vous avez trouvé la combinaison de l' IA\n ");
             choixApresUneFinDePartie();
         } else if (getSaisieJoueur() < getCombinaisonAleatoire() || getSaisieJoueur() > getCombinaisonAleatoire())
-            System.out.println("Vous n'avez pas trouvé la combinaison de l IA\n ");        
+            System.out.println("Vous n'avez pas trouvé la combinaison de l IA\n ");
     }
-    
+
     /**
      * Methode indiquant le nombre de chance utilisees pour l IA
      */
@@ -217,14 +216,15 @@ import fr.escapegame.propriete.ChargerPropriete;
         System.out.println("\n");
         System.out.println("Essai n° " + getChanceUtiliseeIa() + " pour l'Ia");
     }
-  
+
     /**
-     * Methode generant une serie de nombre aleatoire compris dans un intervalle maxi et min
-     * qui s integre dans la methode generationNouvelleCombinaisonIa
+     * Methode generant une serie de nombre aleatoire compris dans un intervalle
+     * maxi et min qui s integre dans la methode generationNouvelleCombinaisonIa
+     * 
      * @param min
      * @param max
      * @return
-     */    
+     */
     public int generationNbreAletoire(int min, int max) {
         if (min == max)
             return min;
@@ -234,7 +234,7 @@ import fr.escapegame.propriete.ChargerPropriete;
 
     /**
      * Methode qui va generer une nouvelle combinaison aleatoire pour l IA en
-     * fonction du resultat des operateurs saisi par le joueur dans la methode 
+     * fonction du resultat des operateurs saisi par le joueur dans la methode
      * saisiOperateur
      */
     public void generationNouvelleCombinaisonIa() {
@@ -260,36 +260,36 @@ import fr.escapegame.propriete.ChargerPropriete;
     }
 
     /**
-     * Methode affichant la nouvelle combinaison de l IA 
+     * Methode affichant la nouvelle combinaison de l IA
      */
     public void nouvelleCombinaisonIa() {
         System.out.println("L'IA propose la nouvelle combinaison " + getNouvelleCombinaisonIa());
         setCombiIaConvertiEnString(getNouvelleCombinaisonIa());
     }
-    
-    /**
-     * Methode permettant au joueur de saisir sous forme d operateur pour modifier a
-     * la hausse ou la baisse la nouvelle combinaison de l IA Mode 
-     */
-    public void saisieOperateur() {
-            Scanner scan = new Scanner(System.in);
-            Pattern operateur = Pattern.compile("[+---=]");
-            System.out.println("Veuillez saisir le résultat sous forme d'opérateur + - ou =");
-            while (!scan.hasNext(operateur)) {
-                if (scan.hasNext()) {
-                    System.out.println("Vous n'avez pas saisi des opérateurs ");
-                    System.out.println("Veuillez à nouveau saisir le résultat sous forme d'opérateur + - ou = ");
-                    String result = scan.nextLine();                     
-                    setResultat(result);
-                }             
-            }
-            String result = scan.nextLine();                     
-            System.out.println("Vous avez saisi " + result);
-            setResultat(result);
-        }
 
     /**
-     * Methode affichant si l' IA a trouvee ou non la combinaison du joueur 
+     * Methode permettant au joueur de saisir sous forme d operateur pour modifier a
+     * la hausse ou la baisse la nouvelle combinaison de l IA Mode
+     */
+    public void saisieOperateur() {
+        Scanner scan = new Scanner(System.in);
+        Pattern operateur = Pattern.compile("[+---=]");
+        System.out.println("Veuillez saisir le résultat sous forme d'opérateur + - ou =");
+        while (!scan.hasNext(operateur)) {
+            if (scan.hasNext()) {
+                System.out.println("Vous n'avez pas saisi des opérateurs ");
+                System.out.println("Veuillez à nouveau saisir le résultat sous forme d'opérateur + - ou = ");
+                String result = scan.nextLine();
+                setResultat(result);
+            }
+        }
+        String result = scan.nextLine();
+        System.out.println("Vous avez saisi " + result);
+        setResultat(result);
+    }
+
+    /**
+     * Methode affichant si l' IA a trouvee ou non la combinaison du joueur
      */
     public void nouvearesultat() {
         if (getNouvelleCombinaisonIa().equals(getCombinaisonSecreteJoueur())) {
@@ -300,59 +300,60 @@ import fr.escapegame.propriete.ChargerPropriete;
             System.out.println("L'IA n'a pas trouvé la combinaison du joueur ");
         }
     }
-    
+
     /**
      * Methode affichant que l'IA et le joueur ont perdus
      */
     public void defaiteIaEtJoueur() {
-        if (!getNouvelleCombinaisonIa().equals(getCombinaisonSecreteJoueur()) && getNbretrs() != getNbEssai()){
-            System.out.println("Fin de partie, le joueur et l' IA ont perdus");   
-            System.out.print("La combinaison de l'IA est ");{
+        if (!getNouvelleCombinaisonIa().equals(getCombinaisonSecreteJoueur()) && getNbretrs() != getNbEssai()) {
+            System.out.println("Fin de partie, le joueur et l' IA ont perdus");
+            System.out.print("La combinaison de l'IA est ");
+            {
                 for (int z = 0; z < getCombinaisonIa().length; z++)
                     if (getSaisieJoueur() != getCombinaisonAleatoire())
                         System.out.print(getCombinaisonIa()[z] + "\n");
+            }
         }
-      }
     }
-    
+
     /**
-     * Methode qui permet de generer la partie 
-     * Une boucle do while est executee tant que le joueur ou l IA n a pas trouve 
-     * la combinaison de son adversaire et tant que le nombre d essais maximum 
-     * n est pas atteint
-     */    
-    public void TentativeDeTrouverLaCombinaisonEntreIaEtJoueur() {     
+     * Methode qui permet de generer la partie Une boucle do while est executee tant
+     * que le joueur ou l IA n a pas trouve la combinaison de son adversaire et tant
+     * que le nombre d essais maximum n est pas atteint
+     */
+    public void TentativeDeTrouverLaCombinaisonEntreIaEtJoueur() {
         introduction();
         saisieCombinaisonSecreteJoueur();
-        combinaisonIaSecrete(); 
+        combinaisonIaSecrete();
         tourIa();
         propositionCombinaisonIa();
         comparaisonDeCombinaisonPourIa();
-        resultatPourIa();         
+        resultatPourIa();
         tourJoueur();
         saisieJoueur();
         comparaisonDeCombinaison();
-        resultatPourJoueur();       
+        resultatPourJoueur();
         do {
-        nouveauTourPourIa();
-        generationNouvelleCombinaisonIa();
-        nouvelleCombinaisonIa();
-        saisieOperateur();
-        nouvearesultat();             
-        tourJoueur();                     
-        saisieJoueur();
-        comparaisonDeCombinaison();
-        resultatPourJoueur(); 
-        defaiteIaEtJoueur();
-        setNbretrs(getNbretrs() + 1);
-        setChanceUtiliseeIa(getChanceUtiliseeIa() + 1);
-        getNbretrs();           
-        }  while (!getNouvelleCombinaisonIa().equals(getCombinaisonSecreteJoueur()) && getNbretrs() != getNbEssai());
-     }
+            nouveauTourPourIa();
+            generationNouvelleCombinaisonIa();
+            nouvelleCombinaisonIa();
+            saisieOperateur();
+            nouvearesultat();
+            tourJoueur();
+            saisieJoueur();
+            comparaisonDeCombinaison();
+            resultatPourJoueur();
+            defaiteIaEtJoueur();
+            setNbretrs(getNbretrs() + 1);
+            setChanceUtiliseeIa(getChanceUtiliseeIa() + 1);
+            getNbretrs();
+        } while (!getNouvelleCombinaisonIa().equals(getCombinaisonSecreteJoueur()) && getNbretrs() != getNbEssai());
+    }
 
-     /** Methode demandant au joueur de saisir si il souhaite recommencer changer ou
+    /**
+     * Methode demandant au joueur de saisir si il souhaite recommencer changer ou
      * quitter le jeu
-     */          
+     */
     public void propositionApresUneFinDePartie() {
         System.out.println();
         System.out.println("Pour poursuivre veuillez choisir entre les 3 modes ci-dessous:");
@@ -361,7 +362,7 @@ import fr.escapegame.propriete.ChargerPropriete;
         System.out.println("2- Changer de mode de jeu");
         System.out.println("3- Quitter le jeu");
     }
-    
+
     /**
      * Methode qui permet au joueur en fonction du choix propose et de sa saisie de
      * faire une nouvelle partie , de retourner dans le menu de selection des modes
@@ -404,10 +405,10 @@ import fr.escapegame.propriete.ChargerPropriete;
             choixApresUneFinDePartie();
         }
     }
-    
+
     /** Methode generant le deroulement du jeu */
-    public void jouer() {       
+    public void jouer() {
         TentativeDeTrouverLaCombinaisonEntreIaEtJoueur();
-        choixApresUneFinDePartie(); 
+        choixApresUneFinDePartie();
     }
 }
